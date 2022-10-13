566cd362d286627802f9274ba87fed02d173c8e5
Daniel Mattox <dmattox10@gmail.com>
2022-10-13 15:36:29 +0700

    Nav update works

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,6 +1,40 @@
+import { useState } from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Header = (props) => {
+
+    const [active, updateActive] = useState(0)
     
+    const setActive = (id) => {
+        updateActive(prevState => id)
+    }
+
+    const navItems = [
+        {
+            id: 0,
+            name: 'About',
+            target: '#about'
+        },
+        {
+            id: 1,
+            name: 'Work',
+            target: '#work'
+        },
+        {
+            id: 2,
+            name: 'Experience',
+            target: '#experience'
+        },
+        {
+            id: 3,
+            name: 'Contact',
+            target: '#contact'
+        },
+        {
+            id: 4,
+            name: 'Resume',
+            target: '#resume'
+        }
+    ]
     return (
         <nav className="navbar navbar-expand-lg fixed-top navbarScroll">
         <div className="container">
@@ -10,23 +44,18 @@ const Header = (props) => {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto">
-                    <li className="nav-item active">
-                        <AnchorLink offset={() => 100} className='nav-link' href='#about'>About</AnchorLink>
-                    </li>
-                    <li className="nav-item">
-                        <AnchorLink offset={() => 100} className='nav-link' href='#work'>Work</AnchorLink>
-                    </li>
-                    <li className="nav-item">
-                        <AnchorLink offset={() => 100} className='nav-link' href='#experience'>Experience</AnchorLink>
-                    </li>
-                    <li className="nav-item">
-                        <AnchorLink offset={() => 100} className='nav-link' href='#contact'>Contact</AnchorLink>
-                    </li>
-                    <li className="nav-item">
-                        <AnchorLink offset={() => 100} className='nav-link' href='/public/Resume.pdf'>Resume</AnchorLink>
-                    </li>
+                {navItems.map((item) => {
+                    return (
+                        <li
+                        key={item.id}
+                        onClick={() => setActive(item.id)}
+                        className={`nav-item ${active == item.id && 'active'}`}
+                        >
+                            <AnchorLink offset={() => 100} className='nav-link' href={item.target}>{item.name}</AnchorLink>
+                        </li>
+                    )
+                    })}
                 </ul>
-                
             </div>
         </div>
     </nav>
