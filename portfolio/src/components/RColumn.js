c08003239b4c7c8b7c2aebf9f12e1301b29e8347
Daniel Mattox <dmattox10@gmail.com>
2022-10-22 09:39:37 +0700

    Love both sides, time for a toggleable loading intro

--- portfolio/src/components/RColumn.js
+++ portfolio/src/components/RColumn.js
@@ -3,8 +3,10 @@ const RColumn = (props) => {
     return (
 
         <div id='secondary' className=''>
-            <div className='section-body'>
-                Column
+            <div className='right'>
+                <div className='email'>
+                    <a href='mailto:dmattox10@gmail.com'>dmattox10@gmail.com</a>
+                </div>
             </div>
         </div>
     )
