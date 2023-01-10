398a9de58266cb713f9a3bf56b34b63c6e4e801b
Daniel Mattox <dmattox10@gmail.com>
2023-01-10 14:07:49 -0500

    Added the config rules to common

--- html/webpack.common.js
+++ html/webpack.common.js
@@ -17,4 +17,24 @@ const path = require('path');
      path: path.resolve(__dirname, 'dist'),
      clean: true,
    },
- };
+   rules: [
+    {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    {
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'sass-loader'
+    ]
+  }
+]
+};
