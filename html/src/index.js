6fd48cca871418a5e82c161e6c5480968a67d49d
Daniel Mattox <dmattox10@gmail.com>
2023-03-25 20:13:11 +0000

    Need to change those scheduling cards

--- html/src/index.js
+++ html/src/index.js
@@ -13,13 +13,6 @@ for (const link of links) {
   link.textContent = `${attrs.part1}@${attrs.part2}.${attrs.part3}`;
 }
 
-// const navItems = document.querySelectorAll('.nav-item')
-// const menuToggle = document.getElementById('navbarSupportedContent')
-// const bsCollapse = new bootstrap.Collapse(menuToggle)
-// navItems.forEach((l) => {
-//     l.addEventListener('click', () => { bsCollapse.toggle() })
-// })
-
 function updateActiveLink() {
   for (const section of sections) {
     const boundingRect = section.getBoundingClientRect();
@@ -37,69 +30,4 @@ function toggleFileDownloaded() {
 }
 
 updateActiveLink()
-
-// // // const modal = document.querySelector(".modal");
-// // // const closeArea = document.querySelector(".close");
-
-// // function toggleModal() {
-// //   modal.classList.toggle("show-modal");
-// // }
-
-// // function hideModal() {
-// //   modal.classList.remove("show-modal");
-// // }
-
-// // function windowOnClick(event) {
-// //   if (event.target === modal) {
-// //     toggleModal();
-// //   }
-// // }
-
-// closeArea.addEventListener("click", hideModal);
-// window.addEventListener("click", windowOnClick);
-
 window.addEventListener('scroll', updateActiveLink);
-
-// let lastScrollY = 0;
-
-// if(!/Mobi/i.test(navigator.userAgent)){
-//   window.addEventListener('scroll', () => {
-//     if (
-//       window.scrollY < lastScrollY &&
-//       window.scrollY > 800 &&
-//       !window.fileDownloaded &&
-//       !window.modalShown
-//     ) {
-//       modal.classList.add("show-modal");
-//       window.modalShown = true;
-//     }
-//     lastScrollY = window.scrollY;
-//   });
-  
-//   window.addEventListener('scrolling_up', function() {
-//     toggleModal();
-//   });
-// }
-
-
-
-// const section = document.querySelector("#about");
-// const backgroundImage = document.querySelector(".background-image");
-// const overlay = document.querySelector(".overlay");
-// const overlayTextContainer = document.querySelector(".overlay-text-container");
-
-// // Create a timeline
-// const tl = gsap.timeline({ defaults: { ease: "linear" } });
-
-// // Add animations to the timeline
-// tl.to(backgroundImage, { y: "-20%", duration: 2 });
-// tl.to(overlay, { y: "-10%", duration: 2 }, "-=2");
-// tl.to(overlayTextContainer, { y: "-5%", duration: 2 }, "-=2");
-
-// // Create a scrollTrigger
-// gsap.utils.scrollTrigger.create({
-//   trigger: section,
-//   start: "top top",
-//   end: "bottom bottom",
-//   animation: tl,
-// });
